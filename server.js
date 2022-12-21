75ad42fb88bf239a2f5a4dc50fd2a37826081c42
Melik Melikyan <110069520+MHMelikyan@users.noreply.github.com>
2022-12-20 20:02:42 -0500

    vehicle

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const vehicleRouts= require('./controllers/vehicle')
 require('dotenv').config()
 const app = express()
+
+//middleware
+app.use(express.json())
+app.use('/vehicle',vehicleRouts)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
@@ -8,10 +14,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.send('about page')
   })
-  
+  // database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('DB connected'))
+.catch(err => console.error(err));
 
 const PORT = process.env.PORT
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
 
-module.exports = app;
+module.exports = app
